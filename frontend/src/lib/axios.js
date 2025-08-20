3fc88d56049c6e94a1b762da2c64ffeb33457f5e
Gyan <gyanbiswal@Gyans-MacBook-Air.local>
2025-08-20 16:57:48 +0530

    frontend axios folder updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,18 @@
-import axios from "axios";
+// import axios from "axios";
 
-// in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
+// // in production, there's no localhost so we have to make this dynamic
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
+
+// const api = axios.create({
+//   baseURL: BASE_URL,
+// });
+
+// export default api;
+
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5002/api"
+    : import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
